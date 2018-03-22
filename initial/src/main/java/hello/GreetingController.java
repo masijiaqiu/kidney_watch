3af03a690c058f993b5f034d46a5b0f96d8c000a
zxmeng <lucasmeng@v1020-wn-171-168.campus-dynamic.uwaterloo.ca>
2018-03-21 22:20:51 -0400

    using spring

---
+++ initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/individual")
+    public String individual(Model model) {
+        return "individual";
+    }
+
+    @GetMapping("/population")
+    public String population(Model model) {
+        return "population";
+    }
+
+    @GetMapping("/work")
+    public String work(Model model) {
+        return "work";
+    }
+}
